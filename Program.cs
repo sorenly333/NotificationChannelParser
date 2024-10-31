dce4de8de31919cfbdc817404d7838385d8f37d6
Soren Ly <110019765+sorenly333@users.noreply.github.com>
2024-11-01 03:27:30 +0700

    new project commit

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NotificationChannelParser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt the user to enter a notification title
+            Console.WriteLine("The notification should be input with []");
+            Console.WriteLine("Enter the notification title:");
+
+            // Read the user's input (notification title) from the console
+            string input = Console.ReadLine();
+
+            // Call the ParseChannels method to analyze the input and get the result
+            string result = ParseChannels(input);
+
+            // Display the parsed result, showing the relevant notification channels
+            Console.WriteLine(result);
+
+            // Wait for the user to press any key before closing the console window
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+        // Method to parse notification title and identify relevant notification channels
+        static string ParseChannels(string title)
+        {
+            // Define a set of valid notification channels
+            HashSet<string> validChannels = new HashSet<string> { "BE", "FE", "QA", "Urgent" };
+
+            // Create a set to store channels detected in the title
+            HashSet<string> detectedChannels = new HashSet<string>();
+
+            // Regular expression to match tags within square brackets, e.g., "[BE]"
+            Regex regex = new Regex(@"\[(.*?)\]");
+
+            // Find all matches in the input title
+            MatchCollection matches = regex.Matches(title);
+
+            // Loop through each match found by the regular expression
+            foreach (Match match in matches)
+            {
+                // Extract the tag name (e.g., "BE" from "[BE]")
+                string tag = match.Groups[1].Value;
+
+                // Check if the tag is one of the valid notification channels
+                if (validChannels.Contains(tag))
+                {
+                    // Add the tag to detected channels if it's valid
+                    detectedChannels.Add(tag);
+                }
+            }
+            // Check if any valid channels were detected
+            if (detectedChannels.Count > 0)
+            {
+                // Format the output to list all detected channels
+                return $"Receive channels: {string.Join(", ", detectedChannels)}";
+            }
+            else
+            {
+                // If no valid channels were detected, return an informative message
+                return "No valid notification channels detected.";
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NotificationChannelParser")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NotificationChannelParser")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("85c0fdb6-664c-4747-8938-b4ae3c2db589")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
